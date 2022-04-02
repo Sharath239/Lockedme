aa50ea6f582b4b4459e17b86bf47535dd2d39f30
Sharath239 <101708489+Sharath239@users.noreply.github.com>
2022-04-02 13:20:47 +0530

    Lockedme.com

---
+++ src/com/lockedme/Test.java
@@ -0,0 +1,13 @@
+package com.lockedme;
+
+public class Test {
+	
+       public static final String path = "D:/4-1"; 
+	
+	public static void main(String[] args) {
+		Display menu = new Display();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+
+}
